91098231ef0de4237b8b0e1f6beed2cf1dc32c5b
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-08-31 20:19:53 +0200

    instalacion de librerias tailwind y lucide

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home'
+
+import { Layout } from './components/layout/Layout'
+import { Home } from './pages/Home';
+import { Explore } from './pages/Explore';
+import { TopRated } from './pages/TopRated'
+import { Favorites } from './pages/Favorites'
 
 function App() {
 
   return (
     <Routes>
-            <Route path="/" element={Home}></Route>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/explore" element={<Explore />}></Route>
+              <Route path="/top-rated" element={<TopRated />}></Route>
+              <Route path="/favorites" element={<Favorites />}></Route>
+            </Route>
     </Routes>
   )
 }
