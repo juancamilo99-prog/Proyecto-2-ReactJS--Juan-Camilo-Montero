f87ceb6015e6bb0685cfc9365ddf8fba480af440
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-09-06 19:15:02 +0200

    feat(favoriteContext) envolvemos toda la app con providder para que todos tengan acceso al useFavoritos

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { FavoriteContext } from './context/FavoriteContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <FavoriteContext> 
+    <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-
+  </StrictMode>
+  </FavoriteContext>,
 )
